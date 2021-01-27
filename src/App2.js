49b3d60316529d0f4136c831c9d5003cf9915d58
Hoyt Emerson <nathan_h_emerson@homedepot.com>
2021-01-26 21:01:50 -0800

    Update app js

--- src/App2.js
+++ src/App2.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import SideMenu from './components/sideMenu'
 import Stream from "./components/streamRender.js";
+import IndustryStream from "./components/streamRenderIndustry.js";
+import AnalyzeStream from "./components/streamRenderAnalyze.js";
+import VisualizeStream from "./components/streamRenderVisualize.js";
+import ModelStream from "./components/streamRenderModel.js";
+import DeployStream from "./components/streamRenderDeploy.js";
 import './App.css'
 import { Column, Row} from 'simple-flexbox';
 import ToggleButtonExample from './components/nav'
@@ -23,7 +28,7 @@ function App2() {
                       <Column >
                         <div className="block ">
                             <div class="card-header"><h5 class='text-light'>Industry</h5></div>
-                            <Row class><div className='p-3'><Stream/></div></Row>
+                            <Row class><div className='p-3'><IndustryStream/></div></Row>
                         </div>
                       </Column>
                     </Row>
@@ -31,7 +36,15 @@ function App2() {
                       <Column >
                         <div className="block ">
                             <div class="card-header"><h5 class='text-light'>Analyze</h5></div>
-                            <Row class><div className='p-3'><Stream/></div></Row>
+                            <Row class><div className='p-3'><AnalyzeStream/></div></Row>
+                        </div>
+                      </Column>
+                    </Row>
+                    <Row horizontal='space-around'>
+                      <Column >
+                        <div className="block ">
+                            <div class="card-header"><h5 class='text-light'>Visualize</h5></div>
+                            <Row class><div className='p-3'><VisualizeStream/></div></Row>
                         </div>
                       </Column>
                     </Row>
@@ -39,7 +52,7 @@ function App2() {
                       <Column>
                         <div className="block ">
                             <div class="card-header" ><h5 class='text-light'>Model</h5></div>
-                            <Row class><div className='p-3'><Stream/></div></Row>
+                            <Row class><div className='p-3'><ModelStream/></div></Row>
                         </div>
                       </Column>
                     </Row>
@@ -47,10 +60,11 @@ function App2() {
                       <Column>
                         <div className="block ">
                             <div class="card-header" ><h5 class='text-light'>Deploy</h5></div>
-                            <Row class><div className='p-3'><Stream/></div></Row>
+                            <Row class><div className='p-3'><DeployStream/></div></Row>
                         </div>
                       </Column>
                     </Row>
+                    
                  
                 </Column>
                 
